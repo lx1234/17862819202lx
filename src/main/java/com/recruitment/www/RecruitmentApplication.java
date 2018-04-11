6dcbf16da220029c27e8f39b13d42ea909869c04
tyytr <17862811537@163.com>
2018-04-11 11:00:16 +0800

    new  project

--- src/main/java/com/recruitment/www/RecruitmentApplication.java
+++ src/main/java/com/recruitment/www/RecruitmentApplication.java
@@ -3,6 +3,12 @@ package com.recruitment.www;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @Author: Gaoyp
+ * @Description: 实现注册demo
+ * @Date: Create in 下午3:23 2018/3/15
+ * @Modified By:
+ */
 @SpringBootApplication
 public class RecruitmentApplication {
 
